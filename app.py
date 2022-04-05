806ec58e2a308b1709814969196c65fb182a93c9
27b <baldassani@tuta.io>
2022-04-04 22:28:46 -0300

    Updating app.py with the new endpoints

--- app.py
+++ app.py
@@ -1,69 +1,84 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, emit
 from dotenv import load_dotenv
 from os import getenv
 from uuid import uuid4
+from user import User, users
+from map import Map
 
-# Config
+
 load_dotenv()
 
 app = Flask(__name__, static_url_path='/')
 
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
 app.config['DEBUG'] = eval(getenv('DEBUG'))
+
 app.config['PORT'] = 5000
 
 socketio = SocketIO(app)
 
-user_list = []
+
+map = Map()
+
+rows = 15
+
+cols = 15
+
+n_value = 0
+
+map.set_map(rows, cols, value=n_value)
 
 
-# Auxiliary functions
-def check_if_username_valid(username: str) -> bool:
-    '''Check if the username is valid to assing.
-    
-    Args:
-        username: The username to assing.
-
-    Returns:
-        bool: If the username is valid.
-    '''
-    for user in user_list:
-        if user['username'] == username:
-            return False
-    return True
-
-def add_user_in_database(user: dict[str, str]) -> None:
-    '''Add user dict in the list of users.
-    
-    Args:
-        dict: With username and secret_key.
-    
-    Returns:
-        None
-    '''
-    user_list.append(user)
+# Aux functions
+def send_map_status_to_clients():
+    map_data = map.get_map()
+    socketio.emit('map', map_data, broadcast=True)
 
 
 # Sockets
 @socketio.on('new_user')
 def register_new_user(username: str):
-    if check_if_username_valid(username):
+    if User.check_if_username_valid(username):
         new_user = {
             'username': username,
-            'secret_key': uuid4().hex
+            'secret_key': uuid4().hex,
+            'position': User.generate_random_position(rows, cols)
         }
-        add_user_in_database(new_user)
+        User.add_user_in_database(new_user)
         emit('new_user', new_user)
     else:
         emit('new_user', 'Username in use.')
 
-    print(user_list)
+
+@socketio.on('map_print_instructions')
+def map_print_instructions():
+    map_data = {'cols': cols, 'rows': rows}
+    emit('map_print_instructions', map_data, broadcast=True)
 
 
-@socketio.on('message')
-def handle_message(data: str):
-    print('received message: ' + data)
+@socketio.on('map')
+def map_print_instructions():
+    map_data = map.get_map()
+    emit('map', map_data, broadcast=True)
+
+
+@socketio.on('user_direction')
+def user_direction(data):
+    username = data['username']
+    secret_key = data['secret_key']
+    x, y = data['data']['point']
+    credentials = User.check_user_credentials(username, secret_key)
+    if credentials and map.check_if_point_in_map(x, y):
+        user = users[int(credentials)]
+        old_x = user['position'][0]
+        old_y = user['position'][1]
+        map.set_point_in_map(old_x, old_y, n_value)  # Clean old point in map
+        user['position'] = [x, y]                    # Set point in user data
+        map.set_point_in_map(x, y, user['username']) # Set new point in map
+        emit('user_direction', [x, y])               # Send new values to user
+        send_map_status_to_clients()                 # Update map for everyone
 
 
 # Endpoints
