c565ecc6c00c582a04ded0f96c8aef3a0c832c2f
27b <baldassani@tuta.io>
2022-05-02 21:45:49 -0300

    Adding .get_point in map.py

--- map.py
+++ map.py
@@ -15,6 +15,10 @@ class Map:
         '''Returns the map of the created instance.'''
         return self.map
 
+    def get_point(self, x, y) -> any:
+        if self.check_if_point_in_map(x, y):
+            return self.map[y][x] 
+
     def check_if_point_in_map(self, x: int, y: int) -> bool:
         '''Check if the point exists in the map.'''
         if x >= 0 and x < len(self.map) and \
@@ -65,6 +69,7 @@ if __name__ == '__main__':
 
     # Check if point in map
     assert map.check_if_point_in_use(0, 2) == True
+    assert map.check_if_point_in_use(0, 99) == False
 
     map.map[0][2] = 99
 
